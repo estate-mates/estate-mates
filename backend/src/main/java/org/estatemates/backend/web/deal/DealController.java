93abed3bf0d6e4aa37a1557c76760186eb2efa42
LEE JINSEOP <jjjp9966@gmail.com>
2024-04-06 02:22:16 +0900

    feat: Swagger를 사용하여 아파트 매매가 조회 API 문서화

--- backend/src/main/java/org/estatemates/backend/web/deal/DealController.java
+++ backend/src/main/java/org/estatemates/backend/web/deal/DealController.java
@@ -1,5 +1,10 @@
 package org.estatemates.backend.web.deal;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.estatemates.backend.service.DealService;
 import org.estatemates.backend.web.dto.DealResponseDto;
@@ -17,8 +22,13 @@ import java.util.List;
 public class DealController {
     private final DealService dealService;
 
+    @Operation(summary = "아파트 매매가 조회", description = "특정 아파트의 매매가를 조회한다.")
+    @ApiResponse(responseCode = "200", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = DealResponseDto.class))
+    })
     @GetMapping("/{apartmentId}")
-    public ResponseEntity<List<DealResponseDto>> findByApartmentId(@PathVariable Long apartmentId) {
+    public ResponseEntity<List<DealResponseDto>> findByApartmentId(
+            @Parameter(description = "아파트 ID", required = true) @PathVariable Long apartmentId) {
         List<DealResponseDto> response = dealService.findByApartmentId(apartmentId);
         return ResponseEntity.ok().body(response);
     }

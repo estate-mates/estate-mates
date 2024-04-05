bbf46d4b06f1243cf30c5fb1d1dde12fca8553eb
LEE JINSEOP <jjjp9966@gmail.com>
2024-04-05 14:10:25 +0900

    refactor: QuerydslPredicateExecutor 인터페이스 제거

--- backend/src/main/java/org/estatemates/backend/domain/deal/DealRepository.java
+++ backend/src/main/java/org/estatemates/backend/domain/deal/DealRepository.java
@@ -1,7 +1,6 @@
 package org.estatemates.backend.domain.deal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface DealRepository extends JpaRepository<Deal, Long>, QuerydslPredicateExecutor<Deal>, DealRepositoryCustom {
+public interface DealRepository extends JpaRepository<Deal, Long>, DealRepositoryCustom {
 }

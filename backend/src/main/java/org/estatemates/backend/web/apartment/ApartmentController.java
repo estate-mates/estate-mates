41ed477b80156cf32666b418506389c6bdbbf3f4
LEE JINSEOP <jjjp9966@gmail.com>
2024-04-06 02:14:21 +0900

    feat: Swagger를 사용하여 아파트 조회 API 문서화

--- backend/src/main/java/org/estatemates/backend/web/apartment/ApartmentController.java
+++ backend/src/main/java/org/estatemates/backend/web/apartment/ApartmentController.java
@@ -1,5 +1,10 @@
 package org.estatemates.backend.web.apartment;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.estatemates.backend.service.ApartmentService;
 import org.estatemates.backend.web.dto.ApartmentResponseDto;
@@ -17,11 +22,16 @@ import java.util.List;
 public class ApartmentController {
     private final ApartmentService apartmentService;
 
+    @Operation(summary = "아파트 조회", description = "지정한 범위 내의 아파트를 조회한다.")
+    @ApiResponse(responseCode = "200", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = ApartmentResponseDto.class))
+    })
     @GetMapping
-    public ResponseEntity<List<ApartmentResponseDto>> findByBounds(@RequestParam double southWestLatitude,
-                                                                   @RequestParam double southWestLongitude,
-                                                                   @RequestParam double northEastLatitude,
-                                                                   @RequestParam double northEastLongitude) {
+    public ResponseEntity<List<ApartmentResponseDto>> findByBounds(
+            @Parameter(description = "남서쪽 지점의 위도", required = true) @RequestParam double southWestLatitude,
+            @Parameter(description = "남서쪽 지점의 경도", required = true) @RequestParam double southWestLongitude,
+            @Parameter(description = "북동쪽 지점의 위도", required = true) @RequestParam double northEastLatitude,
+            @Parameter(description = "북동쪽 지점의 경도", required = true) @RequestParam double northEastLongitude) {
         List<ApartmentResponseDto> response = apartmentService.findByBounds(
                 southWestLatitude, southWestLongitude,
                 northEastLatitude, northEastLongitude);
